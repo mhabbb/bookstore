52d340d7914a7ec7855e39f4869875118961a832
michalhabigier <michalhabigier@gmail.com>
2017-11-27 12:55:19 +0100

    constructor added

--- src/main/java/pl/mh/bookstore/domain/BookCategory.java
+++ src/main/java/pl/mh/bookstore/domain/BookCategory.java
@@ -1,12 +1,21 @@
 package pl.mh.bookstore.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum BookCategory {
-    CRIMINAL,
-    HORROR,
-    DRAMA,
-    SF,
-    ADVENTURE,
-    ROMANCE,
-    FOREIGN,
-    TODO
+    CRIMINAL("Criminal"),
+    HORROR("Horror"),
+    DRAMA("Drama"),
+    SF("SF"),
+    ADVENTURE("Adventure"),
+    ROMANCE("Romance"),
+    FOREIGN("Foreign"),
+    TODO("Temporary value for all");
+
+    private final String name;
+
+    BookCategory(String name) {
+        this.name = name;
+    }
 }
